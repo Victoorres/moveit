bfdfa3a6731cbcec1e50df75373d5e0cbeb93d9d
v.torres <victor.torres@kingspanisoeste.com.br>
2021-09-23 13:52:05 -0300

    ✨ feat: adding ChallengeBox component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
 import Head from 'next/head';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 export default function Home() {
   return (
@@ -14,14 +15,16 @@ export default function Home() {
 
       <ExperienceBar />
 
-      <section>
+      <section style={{ marginTop: '5rem' }}>
         <div>
           <Profile />
           <CompletedChallenges />
           <Countdown />
         </div>
 
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
