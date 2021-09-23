0a0ae65479da98ca18615d69615149affb3f0457
v.torres <victor.torres@kingspanisoeste.com.br>
2021-09-23 14:42:39 -0300

    ✨ feat: adding challenges context

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,10 +1,12 @@
 import { BiAlarm, BiBadgeCheck, BiXCircle } from 'react-icons/bi';
 import styles from '../styles/components/Countdown.module.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 
 let countdownTimeout: NodeJS.Timeout;
 
 export function Countdown() {
+  const { startNewChallenge } = useContext(ChallengesContext);
   /**
    * States
    */
@@ -51,6 +53,7 @@ export function Countdown() {
     } else if (isActive && time === 0) {
       setHasFinished(true);
       setIsActive(false);
+      startNewChallenge();
     }
   }, [isActive, time]);
 
